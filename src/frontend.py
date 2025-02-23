5bfc95f0c3ff6acbc7e80265ad2e2cb3b484811e
fsgregorio <fsgregorio92@gmail.com>
2025-02-23 14:57:53 -0300

    Functional test works

---
+++ src/frontend.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+class ExcelValidatorUI:
+
+    def __init__(self):
+        self.set_page_config()
+        
+        
+    def set_page_config(self):
+        st.set_page_config(
+            page_title="Excel Validator",
+            page_icon="📊",
+            layout="centered",
+            initial_sidebar_state="auto",
+        )
+        
+    def display(self):
+        st.title("Excel Validator")
+        st.write("Upload an Excel file and we will validate it for you.")
+
+    def upload_file(self):
+        uploaded_file = st.file_uploader("Choose a file", type=["xlsx"])
+        if uploaded_file is not None:
+            return uploaded_file
+        return None
+    
+if __name__ == '__main__':
+    app = ExcelValidatorUI()
+    app.display()
